dfdc1311ab1088e148f7d63368f304dc5d0522c1
levan021111984@gmail.com <Nwz-a845@>
2025-03-14 22:10:46 +0700

    Dependency INjection : Add JWT Authentication service

---
+++ Ecommerce.SharedLibrary/DependenceInjection/JWTAuthenticationScheme.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Ecommerce.SharedLibrary.DependenceInjection
+{
+    public static class JWTAuthenticationScheme
+    {
+        public static IServiceCollection AddJWTAuthenticationScheme(this IServiceCollection services, IConfiguration config)
+        {
+            //Add JWT Authentication service
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer("Bearer", options =>
+                {
+                    var key = Encoding.UTF8.GetBytes(config.GetSection("Authentication:Key").Value!);
+                    string issuer = config.GetSection("Authentication:Issuer").Value!;
+                    string audience = config.GetSection("Authentication:Audience").Value!;
+
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = false,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = issuer,
+                        ValidAudience = audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(key)
+                    };
+                });
+            return services;
+        }
+    }
+}
