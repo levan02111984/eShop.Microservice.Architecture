ed8281dfbfd13dda05d686f8b83006ce7dde7e16
levan021111984@gmail.com <Nwz-a845@>
2025-03-17 13:38:15 +0700

    ProductDBContext and ProductRepository

---
+++ Ecommerce.ProductApiSolution/ProductApi.Infrastructure/Data/ProductDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Domain.Entities;
+
+
+namespace ProductApi.Infrastructure.Data
+{
+    public class ProductDbContext(DbContextOptions<ProductDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Ecommerce.ProductApiSolution/ProductApi.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,72 @@
+﻿using Ecommerce.SharedLibrary.Logs;
+using Ecommerce.SharedLibrary.Responses;
+using ProductApi.Application.Interfaces;
+using ProductApi.Domain.Entities;
+using ProductApi.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApi.Infrastructure.Repositories
+{
+    internal class ProductRepository(ProductDbContext context) : IProduct
+    {
+        public async Task<Response> CreateAsync(Product entity)
+        {
+            try
+            {
+                //Checl if existed 
+                var getProduct = await GetByAsync(p => p.Name!.Equals(entity.Name));
+
+                if (getProduct is not null && !string.IsNullOrEmpty(getProduct.Name))
+                {
+                    return new Response(false, $"{entity.Name} already added");
+                }
+                //If Ok add new entity and conver it to Entity
+                var currentEntity = context.Products.Add(entity).Entity;
+                await context.SaveChangesAsync();
+
+                if (currentEntity is not null && currentEntity.Id > 0)
+                    return new Response(true, $"{entity.Name} added to database sucessfully");
+                else
+                    return new Response(false, "Error occurred");
+            }
+            catch (Exception ex)
+            {
+                //Log the original exceotion
+                LogException.LogExceptions(ex);
+
+                //Display friendly message to the client
+                return new Response(false, "Error occurred adding new product");
+            }
+        }
+
+        public Task<Response> DeleteAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> FindByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByAsync(Expression<Func<Product, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> UpdateAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
