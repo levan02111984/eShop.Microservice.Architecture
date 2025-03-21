1ee63d3305744aa94f29e0c2a6c8900c0ea93777
levan021111984@gmail.com <Nwz-a845@>
2025-03-21 11:21:20 +0700

    Add ServiceContainer.cs

---
+++ Ecommerce.ProductApiSolution/ProductApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,36 @@
+﻿using Ecommerce.SharedLibrary.DependenceInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApi.Application.Interfaces;
+using ProductApi.Infrastructure.Data;
+using ProductApi.Infrastructure.Repositories;
+
+namespace ProductApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            //Add database connectivity
+            //Add authentication scheme
+            SharedServiceContainer.AddSharedService<ProductDbContext>(services, config, config["MySerilog:FineName"]);
+
+            //Create Dependency
+            //Register Respoitory 
+            services.AddScoped<IProduct, ProductRepository>();
+           
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            //Register middleware
+            //GLobal Excpetion 
+            SharedServiceContainer.UseSharedPolices(app);
+            return app;
+        }
+
+
+    }
+}
---
+++ Ecommerce.SharedLibrary/DependenceInjection/SharedServiceContainer.cs
@@ -0,0 +1,53 @@
+﻿using Ecommerce.SharedLibrary.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Ecommerce.SharedLibrary.DependenceInjection
+{
+    public static class SharedServiceContainer
+    {
+        public static IServiceCollection AddSharedService<TContext>(IServiceCollection services,IConfiguration config, string filename) where TContext : DbContext
+        {
+            string sqlConnectionString = config.GetConnectionString("eShopConnection")!;
+
+            //Add Generic Database context
+            services.AddDbContext<TContext>(
+                option => option.UseSqlServer(sqlConnectionString, sqlServerOption => sqlServerOption.EnableRetryOnFailure())
+            );
+
+            //configure serilog loggin
+            Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Information()
+                        .WriteTo.Debug()
+                        .WriteTo.Console()
+                        .WriteTo.File(path: $"{filename}-.text",
+                            restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss fff zzz} [{level:u3}] {message:lj} {NewLine} {Exception}",
+                            rollingInterval: RollingInterval.Day)
+                         .CreateLogger();
+
+            //Add Jwt authentication Scheme
+            JWTAuthenticationScheme.AddJWTAuthenticationScheme(services, config);
+            return services;
+        }
+
+        public static IApplicationBuilder UseSharedPolices(this IApplicationBuilder app)
+        {
+            //Use global Exception
+            app.UseMiddleware<GlobalException>();
+
+            //Register middleware to block outsiders API calls
+            //app.UseMiddleware<ListenToOnlyApiGateway>();
+
+            return app;
+        }
+
+
+    }
+
+
+}
