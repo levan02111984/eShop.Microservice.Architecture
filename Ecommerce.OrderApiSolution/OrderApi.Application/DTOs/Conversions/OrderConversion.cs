9f8e2234b7e51762b8e787b7ec93e9e1ec9d0023
levan021111984@gmail.com <Nwz-a845@>
2025-03-22 23:02:33 +0700

    Updated

---
+++ Ecommerce.OrderApiSolution/OrderApi.Application/DTOs/Conversions/OrderConversion.cs
@@ -0,0 +1,49 @@
+﻿using OrderApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Application.DTOs.Conversions
+{
+    public static class OrderConversion
+    {
+        public static Order ToEntity(OrderDTO order) => new()
+        {
+            Id = order.Id,
+            ClientId = order.ClientId,
+            ProductId = order.ProductId,
+            OrderedDate = order.OrderedDate,
+            PurchaseQuantity = order.PurchaseQuantity
+        };
+
+        public static (OrderDTO? , IEnumerable<OrderDTO>?) FromEntity(Order? order, IEnumerable<Order>? orders)
+        {
+            //Return single
+            if(order is not null || orders is null)
+            {
+                var currentOrder = new OrderDTO(
+                    order!.Id,
+                    order.ClientId,
+                    order.ProductId,
+                    order.PurchaseQuantity,
+                    order.OrderedDate
+                );
+
+                return (currentOrder, null);
+            }
+
+            if(order is null || orders is not null)
+            {
+                var currentOrders = orders.Select(p =>
+                        new OrderDTO(p.Id, p.ProductId, p.ClientId, p.PurchaseQuantity, p.OrderedDate
+                    )).ToList();
+
+                return (null, currentOrders);
+            }
+
+            return (null,null);
+        }
+    }
+}
---
+++ Ecommerce.OrderApiSolution/OrderApi.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Application.DTOs
+{
+    public record ProductDTO
+    (
+        int Id,
+        [Required] string Name,
+        [Required] decimal Price,
+        [Required] int Quantity
+    );
+}
